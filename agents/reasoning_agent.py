1e59c9f8028a10827e765370b7ef8ffd24c99b38
dayuwang401 <wangdayu820@gmail.com>
2025-09-10 16:20:00 +0800

    Update reasoning_agent.py

--- agents/reasoning_agent.py
+++ agents/reasoning_agent.py
@@ -6,51 +6,12 @@ import logging
 # 设置日志
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-SYSTEM_PROMPT_R = """You are a Reasoning Agent and need to generate the rollout of solving a certain question.
-
-Rules:
-- When you need to do a calculation or use python, describe all the values needed to write the code in the step,then output the token <code>.  
-- After <code>, the environment will insert <result>...</result> containing the computation result，Continue reasoning based on that result.
-- if the environment insert <result><error></result>, express the previous reasoning step and generate code.
-- When you have the final answer, wrap it in <answer> ... </answer> and stop,the answer should be pure number.
-here are examples.
-Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
-A: There are 15 trees originally. Then there were 21 trees after some more were planted. We need to calculate 21-15<code><result>6</result> So the final answer is <answer>6</answer>
-
-
-Q: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
-A: There are originally 3 cars. 2 more cars arrive. We need to calculate 3+2<code><result>5</result> So the final answer is <answer>5</answer>
-
-
-Q: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
-A: Originally, Leah had 32 chocolates. Her sister had 42. First we calculate 32 + 42.<code><result>74</result> they have 74 in total,they ate 35, we then calculate 74-35<code><result>39</result>. So the final answer is <answer>39</answer>.
-
-
-Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-A: Jason started with 20 lollipops. Then he had 12 after giving some to Denny. So he gave Denny 20 - 12,we need calculate 20-12<code><result>8</result>. So the final answer is <answer>8</answer>.
-
-
-Q: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
-A: Shawn started with 5 toys. If he got 2 toys each from his mom and dad, we need to calculate 5 + 2*2<code><result>9</result>. So the final answer is <answer>9</answer>.
-cod
-
-Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-A: There were originally 9 computers. For each of 4 days, 5 more computers were added. First we calculate the 4*5<code><result>20</result>. Then we calculate 9 + 20<code><result>29</result>. So the final answer is <answer>9</answer>.
-
-
-Q: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
-A: Michael started with 58 golf balls. After losing 23 on tuesday, we need to calculate 58-23<code><result>35</result>. After losing 2 more, we need to calculate 35 - 2<code><result>33</result>. So the final answer is <answer>33</answer>.
-
-
-Q: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
-A: Olivia had 23 dollars. 5 bagels for 3 dollars each will be 5 x 3<code><result>15</result>. So she has 23 - 15<code><result>8<result>So the final answer is <answer>8</answer>.
-
-"""
+SYSTEM_PROMPT_R = """Please integrate natural language reasoning with programs to solve the problem above, and put your final answer within \boxed{}
+every time you generate ```python, the enviroment will generate the python code and insert the output of code. if an error occur,redescribe the reasoning step
+""" 
 
 USER_PROMPT_TEMPLATE = """Problem:
 {question}
-
-Begin reasoning below.
 """
 
 class KeywordStoppingCriteria(StoppingCriteria):
